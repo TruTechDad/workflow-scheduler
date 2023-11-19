2c032107bb223d9ccebb89900853c7caf58984c4
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2023-11-19 12:44:56 -0500

    Added code for the past, present or future for the function I found partially from Stackoverflow

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  $(" .saveBtn").on("click", function() {
+    var timeBlockID = $(this).closest(".time-block").attr("id");
+    var description = timeBlock.find('.description').val();
+    localStorage.setItem(blockId, description);
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -10,8 +15,20 @@ $(function () {
   // useful when saving the description in local storage?
   //
 
-  $(" .saveBtn").on("click", function() {
-    var timeBlockID = $(this).closest(".time-block").attr("id");
+ $(function() {
+  var currentHour = dayjs().format('H'); 
+  $('.time-block').each(function () {
+    var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+    if (blockHour < currentHour) {
+      $(this).removeClass('present future').addClass('past');
+    } else if (blockHour == currentHour) {
+      $(this).removeClass('past future').addClass('present');
+    } else {
+      $(this).removeClass('past present').addClass('future');
+    }
+  });
+ });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -36,4 +53,5 @@ $(function () {
       displayCurrentDayAndTime();
       updatePastPresentFutureClasses();
     }, 1000); 
+  }
 });
